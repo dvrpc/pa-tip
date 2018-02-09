7ca2d4a702cd5c818de5fe9890f6835d6f65e725
Marc Molta <MMolta@dvrpc.org>
2018-02-09 16:45:04 -0500

    map added - width issues but its there

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,14 +1,28 @@
 import Inferno from 'inferno'
 import Component from 'inferno-component'
 import './Map.css'
+import mapboxgl from 'mapbox-gl'
 
 class MapComponent extends Component {
-	//TODO: add props, lifecycle hooks, other stuff
+
+    componentDidMount(){
+        mapboxgl.accessToken = 'pk.eyJ1IjoibW1vbHRhIiwiYSI6ImNqZDBkMDZhYjJ6YzczNHJ4cno5eTcydnMifQ.RJNJ7s7hBfrJITOBZBdcOA'
+        this.map = new mapboxgl.Map({
+            container: this.tipMap,
+            //TODO: look into why the width is so whack
+            style: 'mapbox://styles/mapbox/streets-v9',
+            center: [-75.2273, 40.071],
+            zoom: 8.82
+        })
+    }
+
+    componentWillUnmount(){
+        this.map.remove()
+    }
+
 	render(){
 		return(
-			<div className="mapComponent">
-				<h1> I will become a MapComponent componenet I am 40% width</h1>
-			</div>
+            <div className="map" ref={e => this.tipMap = e} />
 		)
 	}
 }
