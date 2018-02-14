0fb060378e763d07a1206a793224c2d30f934d4a
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2018-02-14 16:12:59 -0500

    Add thumbnails to Tiles

--- src/components/tiles/Tiles.js
+++ src/components/tiles/Tiles.js
@@ -1,76 +1,35 @@
 import Inferno from 'inferno'
 import Component from 'inferno-component'
+import Polyline from '@mapbox/polyline'
 import './Tiles.css'
 
 class Tile extends Component {
 	//TODO: add props, lifecycle hooks, other stuff
-	render(){
-		return(
+    render(){
+        let path
+        if (this.props.data.geometry.type === 'LineString') {
+            path = `&path=color:0x${this.props.color}ff|weight:8|enc:${Polyline.fromGeoJSON(this.props.data)}`
+        }
+        else if (this.props.data.geometry.type === 'MultiLineString') {
+            path = this.props.data.geometry.coordinates.map(c => `&path=color:0x${this.props.color}ff|weight:8|enc:${Polyline.fromGeoJSON({type:'LineString',coordinates:c})}`).join('')
+        }
+        else if (this.props.data.geometry.type === 'Point') {
+            path = `&markers=color:0x${this.props.color}|${this.props.data.geometry.coordinates[1]},${this.props.data.geometry.coordinates[0]}`
+        }
+        else if (this.props.data.geometry.type === 'Polygon') {
+            path = this.props.data.geometry.coordinates.map(c => `&path=color:0x${this.props.color}ff|fillcolor:0x${this.props.color}66|weight:4|enc:${Polyline.fromGeoJSON({type:'LineString',coordinates:c})}`).join('')
+        }
+        return(
 			<div className="tile">
-				<h1> I Am a Project Title</h1>
-                <div className="details">
-                    <p><strong>AQ Code: </strong>S19</p>
-                    <p><strong>County: </strong>Burlington</p>
-                    <p><strong>MCD: </strong>Various</p>
+				<div class="tile-caption">
+                    <small>{this.props.data.properties.MPMS_ID}</small>
+                    <h2>{this.props.data.properties.ROAD_NAME}</h2>
+                    <p>{this.props.data.properties.CTY} County, PA</p>
                 </div>
-                <p className="description">
-                    There are dozens of us! DOZENS! It looks like you've been looking for dragons… in the future. These are my awards, Mother. From Army. The seal is for marksmanship, and the gorilla is for sand racing...
-                </p>
-                <table>
-                    <thead>
-                        <th>Phase</th>
-                        <th>Fund</th>
-                        <th className="table-num">2017</th>
-                        <th className="table-num">2018</th>
-                        <th className="table-num">2019</th>
-                        <th className="table-num">2020</th>
-                        <th className="table-num">2021-2024</th>
-                        <th className="table-num">2025-2028</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>ROW</td>
-                            <td className="table-num">185</td>
-                            <td className="table-num">0</td>
-                            <td className="table-num">23</td>
-                            <td className="table-num">14</td>
-                            <td className="table-num">10</td>
-                            <td className="table-num">85</td>
-                            <td className="table-num">1</td>
-                        </tr>
-                        <tr>
-                            <td>PEFR</td>
-                            <td className="table-num">185</td>
-                            <td className="table-num">0</td>
-                            <td className="table-num">23</td>
-                            <td className="table-num">14</td>
-                            <td className="table-num">10</td>
-                            <td className="table-num">85</td>
-                            <td className="table-num">1</td>
-                        </tr>
-                        <tr>
-                            <td>SNKR</td>
-                            <td className="table-num">185</td>
-                            <td className="table-num">0</td>
-                            <td className="table-num">23</td>
-                            <td className="table-num">14</td>
-                            <td className="table-num">10</td>
-                            <td className="table-num">85</td>
-                            <td className="table-num">1</td>
-                        </tr>
-                        <tr>
-                            <td>SNKR</td>
-                            <td className="table-num">185</td>
-                            <td className="table-num">0</td>
-                            <td className="table-num">23</td>
-                            <td className="table-num">14</td>
-                            <td className="table-num">10</td>
-                            <td className="table-num">85</td>
-                            <td className="table-num">1</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <button>Comments</button>
+                <div class="tile-img">
+                    <img src={`https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyAvK54P-Pb3skEYDLFVoRnSTLtRyG5GJ6U&size=300x250&maptype=hybrid${path}`} />
+                </div>
+                <a href="#" class="tile-link"></a>
             </div>
 		)
 	}
