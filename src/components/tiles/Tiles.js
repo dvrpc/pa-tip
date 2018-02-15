35f8cb8080e8c2841e79256b50311c7404479c2a
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2018-02-15 12:59:54 -0500

    Add prettier precommit hook
    This will totally change blame history when files are updated for the first time

--- src/components/tiles/Tiles.js
+++ src/components/tiles/Tiles.js
@@ -1,54 +1,82 @@
-import Inferno from 'inferno'
-import Component from 'inferno-component'
-import Polyline from '@mapbox/polyline'
-import './Tiles.css'
+import Inferno from "inferno";
+import Component from "inferno-component";
+import Polyline from "@mapbox/polyline";
+import "./Tiles.css";
 
 class Tile extends Component {
-	//TODO: add props, lifecycle hooks, other stuff
-    constructor(props){
-        super(props)
-        this.state = {
-            height: 300,
-            width: 300
-        }
-    }
-    componentDidMount(){
-        const tile = document.querySelector('.tile')
-        this.setState({
-            height: tile.clientHeight,
-            width: tile.clientWidth
-        })
-    }
+  //TODO: add props, lifecycle hooks, other stuff
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 300,
+      width: 300
+    };
+  }
+  componentDidMount() {
+    const tile = document.querySelector(".tile");
+    this.setState({
+      height: tile.clientHeight,
+      width: tile.clientWidth
+    });
+  }
 
-    render(){
-        let path;
-        if (this.props.data.geometry.type === 'LineString') {
-            path = `&path=color:0x${this.props.color}ff|weight:8|enc:${Polyline.fromGeoJSON(this.props.data)}`
-        }
-        else if (this.props.data.geometry.type === 'MultiLineString') {
-            path = this.props.data.geometry.coordinates.map(c => `&path=color:0x${this.props.color}ff|weight:8|enc:${Polyline.fromGeoJSON({type:'LineString',coordinates:c})}`).join('')
-        }
-        else if (this.props.data.geometry.type === 'Point') {
-            path = `&markers=color:0x${this.props.color}|${this.props.data.geometry.coordinates[1]},${this.props.data.geometry.coordinates[0]}`
-        }
-        else if (this.props.data.geometry.type === 'Polygon') {
-            path = this.props.data.geometry.coordinates.map(c => `&path=color:0x${this.props.color}ff|fillcolor:0x${this.props.color}66|weight:4|enc:${Polyline.fromGeoJSON({type:'LineString',coordinates:c})}`).join('')
-        }
-        //TODO: replace this API key with a process.ENV secret
-        const background = `https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyAvK54P-Pb3skEYDLFVoRnSTLtRyG5GJ6U&size=${this.state.width}x${this.state.height}&maptype=hybrid${path}`
+  render() {
+    let path;
+    if (this.props.data.geometry.type === "LineString") {
+      path = `&path=color:0x${
+        this.props.color
+      }ff|weight:8|enc:${Polyline.fromGeoJSON(this.props.data)}`;
+    } else if (this.props.data.geometry.type === "MultiLineString") {
+      path = this.props.data.geometry.coordinates
+        .map(
+          c =>
+            `&path=color:0x${
+              this.props.color
+            }ff|weight:8|enc:${Polyline.fromGeoJSON({
+              type: "LineString",
+              coordinates: c
+            })}`
+        )
+        .join("");
+    } else if (this.props.data.geometry.type === "Point") {
+      path = `&markers=color:0x${this.props.color}|${
+        this.props.data.geometry.coordinates[1]
+      },${this.props.data.geometry.coordinates[0]}`;
+    } else if (this.props.data.geometry.type === "Polygon") {
+      path = this.props.data.geometry.coordinates
+        .map(
+          c =>
+            `&path=color:0x${this.props.color}ff|fillcolor:0x${
+              this.props.color
+            }66|weight:4|enc:${Polyline.fromGeoJSON({
+              type: "LineString",
+              coordinates: c
+            })}`
+        )
+        .join("");
+    }
+    //TODO: replace this API key with a process.ENV secret
+    const background = `https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyAvK54P-Pb3skEYDLFVoRnSTLtRyG5GJ6U&size=${
+      this.state.width
+    }x${this.state.height}&maptype=hybrid${path}`;
 
-        return(
-		<div className="tile" style={`background: url(${background})`}>
-            <div className="tile-caption">
-              <h2 className="tile-caption-text">{this.props.data.properties.ROAD_NAME}</h2>
-              <small className="tile-caption-text">AQ Code: {this.props.data.properties.MPMS_ID}</small>
-              <p className="tile-caption-text">{this.props.data.properties.CTY} County, PA</p>
-            </div>
-            <a href="#" class="tile-link"></a>
+    return (
+      <div className="tile" style={`background: url(${background})`}>
+        <div className="tile-caption">
+          <h2 className="tile-caption-text">
+            {this.props.data.properties.ROAD_NAME}
+          </h2>
+          <small className="tile-caption-text">
+            AQ Code: {this.props.data.properties.MPMS_ID}
+          </small>
+          <p className="tile-caption-text">
+            {this.props.data.properties.CTY} County, PA
+          </p>
         </div>
-		)
-	}
-
+        <a href="#" class="tile-link" />
+      </div>
+    );
+  }
 }
 
-export default Tile
+export default Tile;
