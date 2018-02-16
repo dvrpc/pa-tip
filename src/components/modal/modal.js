976a40ab67edff41ea7bc26c1b4f03459cb12920
Marc Molta <MMolta@dvrpc.org>
2018-02-16 11:27:47 -0500

    custom modal styles v1

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -2,7 +2,12 @@ import Inferno from 'inferno'
 import Component from 'inferno-component'
 import swal from 'sweetalert2'
 
-// issue: swal is a function that I was trying to just render as an element
+// TODO: include the HTML. Data table, full description, comment text area, ability to print(?)
+// TODO: update URL when a modal pops up 
+// FOOTER: it's a text box so it doesn't need to come in from props. can jsut be made right here
+
+const footer = '<h2>Add a comment </h2><textarea></textarea><input type="submit" value="submit">'
+
 class Modal extends Component{
     constructor(props){
         super(props)
@@ -10,12 +15,15 @@ class Modal extends Component{
 
     render(){
         return(
-            <div>
+            <div id="modal">
                 {
                     swal({
                         title: this.props.title,
+                        width: '50%',
+                        padding: 80,
                         html: this.props.html,
-                        type: 'info'
+                        showConfirmButton: false,
+                        footer: footer
                     })
                 }
             </div>
--- src/components/tiles/Tiles.js
+++ src/components/tiles/Tiles.js
@@ -5,7 +5,7 @@ import Polyline from '@mapbox/polyline'
 import swal from 'sweetalert2'
 
 import './Tiles.css'
-import Modal from '../modal/modal.js'
+import Modal from '../modal/Modal.js'
 
 const clickModal = instance => instance.setState({modalClicked: true})
 
@@ -30,6 +30,7 @@ class Tile extends Component {
 
     render(){
         let path;
+        console.log('props data properties ', this.props.data.properties)
         if (this.props.data.geometry.type === 'LineString') {
             path = `&path=color:0x${this.props.color}ff|weight:8|enc:${Polyline.fromGeoJSON(this.props.data)}`
         }
@@ -44,10 +45,11 @@ class Tile extends Component {
         }
         //TODO: replace this API key with a process.ENV secret
         const background = `https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyAvK54P-Pb3skEYDLFVoRnSTLtRyG5GJ6U&size=${this.state.width}x${this.state.height}&maptype=hybrid${path}`
-
+        const table = '<table><thead><th>Phase</th><th>Fund</th></thead><tbody><tr><td>CON</td><td>TOLL</td></tr><tr><td>DRF</td><td>TAU</td></tr></tbody></table>'
+        const description = '<p>this is the <strong>text</strong> of the modal it will eventually <em>be replaced by the actual description</em> of the project, but the JSON being used at the moment doesnt have descriptions so im just writing <strong>nonsense</strong>. shouldve just used lorum ipsum</p>'
         return(
     		<div className="tile" onClick={ linkEvent(this, clickModal) } style={`background: url(${background})`}>
-                {this.state.modalClicked ? <Modal title={this.props.data.properties.ROAD_NAME} html={'this is the <strong>text</strong> of the modal yay cool wow yes'} />: null}
+                {this.state.modalClicked ? <Modal title={this.props.data.properties.ROAD_NAME} html={`${description} ${table}`} />: null}
                 <div className="tile-caption">
                   <h2 className="tile-caption-text">{this.props.data.properties.ROAD_NAME}</h2>
                   <small className="tile-caption-text">AQ Code: {this.props.data.properties.MPMS_ID}</small>
