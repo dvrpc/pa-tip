27b034bfa43f9ceefaa3b9350a96e767db1c4b8b
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2018-04-20 10:31:23 -0400

    Add geojson layers to map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -9,7 +9,30 @@ import "./Map.css";
 class MapComponent extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      layers: {
+        ipd: false,
+        cmp: false,
+        connections: false,
+        freight: false
+      }
+    };
+
+    this.map = new mapboxgl.Map();
   }
+
+  updateLayerVisibility = (selectedLayer, layers) => {
+    if (selectedLayer !== null)
+      this.state[selectedLayer] = !this.state[selectedLayer];
+    Object.keys(layers).forEach(layer =>
+      this.map.setLayoutProperty(
+        layer,
+        "visibility",
+        this.state.layers[layer] ? "visible" : "none"
+      )
+    );
+  };
 
   componentDidMount() {
     //TODO: replace the accessToken with a process.ENV variable
@@ -23,6 +46,105 @@ class MapComponent extends Component {
       center: this.props.center || [-75.1633, 39.9522],
       zoom: 13
     });
+
+    this.map.on("load", () => {
+      this.map.addSource("IPD", {
+        type: "geojson",
+        data:
+          "https://opendata.arcgis.com/datasets/ab586640e7ab40e58c0615f9355cb35a_0.geojson"
+      });
+      this.map.addLayer(
+        {
+          id: "ipd",
+          type: "fill",
+          source: "IPD",
+          paint: {
+            "fill-color": [
+              "interpolate",
+              ["linear"],
+              ["get", "IPD_Score"],
+              9,
+              "#ffffd9",
+              13,
+              "#edf8b1",
+              15,
+              "#c7e9b4",
+              17,
+              "#7fcdbb",
+              19,
+              "#41b6c4",
+              21,
+              "#1d91c0",
+              24,
+              "#225ea8",
+              27,
+              "#253494",
+              30,
+              "#081d58"
+            ],
+            "fill-opacity": 0.5
+          }
+        },
+        "waterway-label"
+      );
+
+      this.map.addSource("CMP", {
+        type: "geojson",
+        data:
+          "https://opendata.arcgis.com/datasets/80cc2057a84f4b63b3745eaa46417507_1.geojson"
+      });
+      this.map.addLayer(
+        {
+          id: "cmp",
+          type: "fill",
+          source: "CMP",
+          paint: {
+            "fill-color": "#0078ae",
+            "fill-opacity": 0.5
+          }
+        },
+        "waterway-label"
+      );
+
+      this.map.addSource("Connections", {
+        type: "geojson",
+        data:
+          "https://opendata.arcgis.com/datasets/75a9bee3b74e44c791388a2cd775f3b7_0.geojson"
+      });
+      this.map.addLayer(
+        {
+          id: "connections",
+          type: "fill",
+          source: "Connections",
+          paint: {
+            "fill-color": "#0078ae",
+            "fill-opacity": 0.5
+          }
+        },
+        "waterway-label"
+      );
+
+      this.map.addSource("Freight", {
+        type: "geojson",
+        data:
+          "https://opendata.arcgis.com/datasets/ca3d5b2b76514ca29a038714f0fa6136_0.geojson"
+      });
+      this.map.addLayer(
+        {
+          id: "freight",
+          type: "fill",
+          source: "Freight",
+          paint: {
+            "fill-color": "#0078ae",
+            "fill-opacity": 0.5
+          }
+        },
+        "waterway-label"
+      );
+
+      this.updateLayerVisibility(null, this.state.layers);
+    });
+
     updateBounds(this);
     updateMarkers(this);
   }
@@ -47,7 +169,28 @@ class MapComponent extends Component {
   }
 
   render() {
-    return <div className="map" ref={e => (this.tipMap = e)} />;
+    return (
+      <div className="map" ref={e => (this.tipMap = e)}>
+        <nav
+          style="position:absolute; z-index:99999; background: rgba(255,255,255,.75);margin-left:10px;margin-top:10px;"
+          className="layer-menu"
+        >
+          {Object.keys(this.state.layers).map(layer => {
+            console.log(layer);
+            return (
+              <a
+                style="display:block;text-decoration:none;margin:3px 5px 3px 20px"
+                href="#"
+                className={this.state[layer] ? "active" : ""}
+                onClick={this.updateLayerVisibility(layer, this.state.layers)}
+              >
+                {layer}
+              </a>
+            );
+          })}
+        </nav>
+      </div>
+    );
   }
 }
 
